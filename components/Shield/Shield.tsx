16d12de168e05228f1799b4d1b1edc04e1e0359e
Even <cbhrat50@gmail.com>
2024-02-13 16:55:22 +0530

    add shield tag

--- components/Shield/Shield.tsx
+++ components/Shield/Shield.tsx
@@ -13,49 +13,53 @@ const Shield = ({ onClick, img, onRegister, onShare, title }: Props) => {
   const [isActive, setIsActive] = useState(false);
   return (
     <>
-      <div
-        onClick={() => {
-          onClick();
-          setIsActive(true);
-        }}
-        className={style["shield"]}
-      >
-        <div className={style["shieldFront"]}></div>
-        <div className={style["shieldBack"]}>
-          <div className="w-[65%] h-[65%] flex flex-col bg-white rounded-xl m-auto">
-            <img
-              src={img}
-              className="object-contain h-70%"
-              alt="no img found!"
-            />
-            <div className="">
-              <h4 className="">{title}</h4>
-              <div className="flex gap-3 px-1">
-                <button
-                  className="px-4 py-2 bg-yellow-300 rounded-md hover:bg-yellow-400 transition-all duration-150"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onRegister();
-                  }}
-                >
-                  Register Now
-                </button>
+      <div className="flex justify-center items-center flex-col">
+        <div
+          onClick={() => {
+            onClick();
+            setIsActive(true);
+          }}
+          className={style["shield"]}
+        >
+          <div className={style["shieldFront"]}></div>
+          <div className={style["shieldBack"]}>
+            <div className="w-[65%] h-[65%] flex flex-col bg-white rounded-xl m-auto">
+              <img
+                src={img}
+                className="object-contain h-70%"
+                alt="no img found!"
+              />
+              <div className="">
+                <h4 className="">{title}</h4>
+                <div className="flex gap-3 px-1">
+                  <button
+                    className="px-4 py-2 bg-yellow-300 rounded-md hover:bg-yellow-400 transition-all duration-150"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onRegister();
+                    }}
+                  >
+                    Register Now
+                  </button>
 
-                <button
-                  className="px-4 py-2 bg-blue-300 text-white rounded-md hover:bg-blue-400 transition-all duration-150"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    onShare();
-                  }}
-                >
-                  Share Now
-                </button>
+                  <button
+                    className="px-4 py-2 bg-blue-300 text-white rounded-md hover:bg-blue-400 transition-all duration-150"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      onShare();
+                    }}
+                  >
+                    Share Now
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="h-20 w-72">
+          <img src="/tags/ai-tag.png" alt="" className="w-full" />
+        </div>
       </div>
-      <div className=""></div>
     </>
   );
 };
